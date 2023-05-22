83b1853b9fe38afeaaa81c443e0ee27c27fda0c8
jhonseloi <jhons.eloi@gmail.com>
2023-05-22 19:31:36 -0300

    atualização

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade de seu veículo é ${vel}.`)
+if (vel > 60) { // CONDIÇÃO SIMPLES
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança!')
