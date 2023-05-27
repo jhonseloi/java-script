27052a8e329cceddc36a1225175270134ceaeeb4
jhonseloi <jhons.eloi@gmail.com>
2023-05-27 09:03:54 -0300

    atualização

--- aula13/ex014/script.js
+++ aula13/ex014/script.js
@@ -5,17 +5,14 @@ function carregar() {
     var hora = agora.getHours()
     msg.innerHTML = (`Agora são ${hora} horas.`)
 
-    if (hora >= 0 && hora < 6) {
+    if (hora >= 0 && hora < 6 || hora >= 18) {
         img.src = 'fotonoite.png'
         document.body.style.background = '#515154'
     } else if (hora >= 6 && hora < 12) {
         img.src = 'fotomanha.png'
         document.body.style.background = '#e2cd9f'
-    } else if (hora > 12 && hora < 18) {
+    } else {
         img.src = 'fototarde.png'
         document.body.style.background = '#b9846f'
-    } else {
-        img.src = 'fotonoite.png'
-        document.body.style.background = '#515154'
     }
 }
