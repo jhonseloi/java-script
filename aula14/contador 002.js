534690ccd39b252e701d19fd2425eace0f605ba9
jhonseloi <jhons.eloi@gmail.com>
2023-06-13 10:50:46 -0300

    atualização

--- aula14/contador 002.js
+++ aula14/contador 002.js
@@ -1,5 +1,5 @@
 var contador = 1
-do { // Estrutura lógico com teste de repetição no final
+do { // Estrutura de repetição com teste lógico no final
     console.log(`Passo ${contador}`)
     contador++
 } while (contador <=6)
