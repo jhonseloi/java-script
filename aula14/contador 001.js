12984e14f0433c6d28f01d7bfaf1d9c1ef61f3e6
jhonseloi <jhons.eloi@gmail.com>
2023-06-13 12:12:11 -0300

    atualização

--- aula14/contador 001.js
+++
@@ -1,5 +0,0 @@
-var contador = 1
-while (contador <=6) { // Estrutura de repetição com teste lógico no início
-    console.log(`Passo ${contador}`)
-    contador++
-}
--- aula14/contador 002.js
+++
@@ -1,5 +0,0 @@
-var contador = 1
-do { // Estrutura de repetição com teste lógico no final
-    console.log(`Passo ${contador}`)
-    contador++
-} while (contador <=6)
