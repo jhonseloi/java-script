fff7c5f9c84167d213f3ec48680d1db0b0ba9883
jhonseloi <jhons.eloi@gmail.com>
2023-06-19 11:50:26 -0300

    atualização

--- aula08/ex016/script.js
+++ aula08/ex016/script.js
@@ -1,15 +1,27 @@
-function contador() {
+function contar() {
     var inicio = document.getElementById('txtini')
     var fim = document.getElementById('txtfim')
     var passo = document.getElementById('txtpas')
     var res = document.getElementById('res')
-    if (Number(inicio.value) == 0 || Number(fim.value) <= inicio.value) {
-        res.innerHTML = ('<strong>Impossível contar!</strong>')
-    } else if (Number(passo.value) == 0) {
-        window.alert('[ERRO]O passo só pode ser dado a partir de 1!')
+    if (inicio.value == 0 || fim.value == 0 || ) {
+        res.innerHTML = 'Impossível contar...'
     } else {
-        for (c = Number(inicio.value); c <= Number(fim.value); c+= Number(passo.value)) {
-            res.innerHTML = (`${contagem}`)
+        res.innerHTML = 'Contando: <br>'
+        i = Number(inicio.value)
+        f = Number(fim.value)
+        p = Number(passo.value) 
+        if (p <= 0) {
+            p = 1
+            window.alert('Passo inválido! Considere a partir do PASSO 1')
+        } else if (i < f) {
+            for (c = 1; c <= f; c += p) {
+                res.innerHTML += `${c}\u{1F449} `
+            }
+        } else {
+            for (c = 1; c >= f; c -= p) {
+                res.innerHTML += `${c}\u{1F449} `
+            }
         }
     }
+    res.innerHTML += '\u{1F3C1}'
 }
