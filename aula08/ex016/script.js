4f736df0a215e66c2a65f8873f32a8eed63c9d55
jhonseloi <jhons.eloi@gmail.com>
2023-06-19 12:28:39 -0300

    atualização

--- aula08/ex016/script.js
+++ aula08/ex016/script.js
@@ -3,25 +3,26 @@ function contar() {
     var fim = document.getElementById('txtfim')
     var passo = document.getElementById('txtpas')
     var res = document.getElementById('res')
-    if (inicio.value == 0 || fim.value == 0 || ) {
-        res.innerHTML = 'Impossível contar...'
+    if (inicio.value.length == 0 || fim.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
     } else {
         res.innerHTML = 'Contando: <br>'
         i = Number(inicio.value)
         f = Number(fim.value)
-        p = Number(passo.value) 
+        p = Number(passo.value)
         if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO = 1')
             p = 1
-            window.alert('Passo inválido! Considere a partir do PASSO 1')
-        } else if (i < f) {
-            for (c = 1; c <= f; c += p) {
+        }
+        if (i < f) {
+            for (c = i; c <= f; c += p) {
                 res.innerHTML += `${c}\u{1F449} `
             }
         } else {
-            for (c = 1; c >= f; c -= p) {
+            for (c = i; c >= f; c -= p) {
                 res.innerHTML += `${c}\u{1F449} `
             }
         }
+        res.innerHTML += '\u{1F3C1}'
     }
-    res.innerHTML += '\u{1F3C1}'
 }
