a76b864168d81dcb61efc93bc65ac755c87ccbd6
jhonseloi <jhons.eloi@gmail.com>
2023-07-12 11:54:43 -0300

    atualização

---
+++ ex018/script.js
@@ -0,0 +1,6 @@
+let num = document.querySelector('input#form_num')
+let lista = document.querySelector('select#form_lista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function adicionar() {}
