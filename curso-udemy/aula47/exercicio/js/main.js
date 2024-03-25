519626a52dc6b10ff4dc9dbdf5b8a4f2c3eb0ca6
jhonseloi <jhons.eloi@gmail.com>
2024-03-25 12:28:08 -0300

    atualização

--- curso-udemy/aula47/exercicio/js/main.js
+++ curso-udemy/aula47/exercicio/js/main.js
@@ -2,29 +2,25 @@ const relogio = document.querySelector('.relogio')
 const iniciar = document.querySelector('.iniciar')
 const pausar = document.querySelector('.pausar')
 const zerar = document.querySelector('.zerar')
-let intervalo
+
 let segundos = 0
+let intervalo
 
 function atualizarRelogio() {
-    const tempo = new Date(segundos * 1000)
-    const horas = tempo.getUTCHours()
-    const minutos = tempo.getMinutes()
-    const segundosFormatados = tempo.getSeconds()
+    const data = new Date(segundos * 1000)
+    const horas = data.getUTCHours()
+    const minutos = data.getMinutes()
+    const segundosFormatados = data.getSeconds()
 
-    const tempoFormatado = `${horas < 10 ? '0' + horas : horas}:${minutos < 10 ? '0' + minutos : minutos}:${segundosFormatados < 10 ? '0' + segundosFormatados: segundosFormatados}`
-    relogio.innerHTML = tempoFormatado
+    relogio.innerHTML = `${horas < 10 ? '0' + horas : horas}:${minutos < 10 ? '0' + minutos : minutos}:${segundosFormatados < 10 ? '0' + segundosFormatados : segundosFormatados}`
     relogio.style.color = 'black'
 }
 
-function iniciarCronometro() {
+iniciar.addEventListener('click', function() {
     intervalo = setInterval(function() {
         segundos++
         atualizarRelogio()
     }, 1000)
-}
-
-iniciar.addEventListener('click', function() {
-    iniciarCronometro()
 })
 
 pausar.addEventListener('click', function() {
