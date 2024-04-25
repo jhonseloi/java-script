2245c774e3480291fcf3741605911027eaf8e01b
jhonseloi <jhons.eloi@gmail.com>
2024-04-25 12:44:40 -0300

    atualização

---
+++ curso-udemy/aula63/splice001.js
@@ -0,0 +1,61 @@
+//                -5         -4       -3        -2         -1    
+//                 0          1        2         3          4
+const nomes = ['Leandro', 'Silvio', 'Joana', 'Ricardo', 'Amanda']
+
+// variavel.splice(indice, delete, add elem1, add elem2, add elem3)
+
+const deletePositivo = () => {
+    const n = [...nomes]
+    const removidos = n.splice(2, 2)
+    console.log(removidos)
+}
+
+const deleteNegativo = () => {
+    const n = [...nomes]
+    const removidos = n.splice(-5, 2)
+    console.log(removidos)
+}
+
+const deleteRestoFinal = () => {
+    const n = [...nomes]
+    const removidos = n.splice(2, Number.MAX_VALUE)
+    console.log(removidos)
+}
+
+const simularShift = () => {
+    const n = [...nomes] 
+    n.splice(0, 1)
+    console.log(n)
+}
+
+const simularPop = () => {
+    const n = [...nomes]
+    n.splice(-1, 1)
+    console.log(n)
+}
+
+const simularUnshift = () => {
+    const n = [...nomes]
+    n.splice(0, 0, 'LEON')
+    console.log(n)
+}
+
+const simularPush = () => {
+    const n = [...nomes]
+    n.splice(n.length, 0, 'DIOMEDES')
+    console.log(n)
+}
+
+deletePositivo()
+console.log('--------')
+deleteNegativo()
+console.log('--------')
+deleteRestoFinal()
+console.log('--------')
+simularShift()
+console.log('--------')
+simularPop()
+console.log('--------')
+simularUnshift()
+console.log('--------')
+simularPush()
