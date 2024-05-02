a45a059a39e1749b4ae4201f81151eabf7ac0c43
jhonseloi <jhons.eloi@gmail.com>
2024-05-01 21:13:40 -0300

    atualização

---
+++ curso-udemy/aula65/filter-array.js
@@ -0,0 +1,27 @@
+// Filter -> Sempre vai retornar uma array com a mesma quantidade de elementos ou menos
+
+function filter1() {
+    // Retornar número maior que 10 (versão estendida)
+    const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+    function filterCallback(valor) {
+        if (valor > 10) {
+            return valor
+        } else {
+            return
+        }
+    } 
+
+    const numerosFiltrados = numeros.filter(filterCallback)
+    console.log(numerosFiltrados)
+}
+filter1()
+
+const filter2 = () => {
+    // Retornar número maior que 10 (versão encurtada)
+    const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+    
+    const numerosFiltrados = numeros.filter(valor => valor > 10)
+    console.log(numerosFiltrados)
+}
+filter2()
