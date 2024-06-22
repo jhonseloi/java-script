de72c639d2e61dc5eb149852babac5233d6908ab
jhonseloi <jhons.eloi@gmail.com>
2024-06-22 10:02:42 -0300

    atualização

---
+++ curso-udemy/aula86/classe-validaCPF.js
@@ -0,0 +1,57 @@
+// 705.484.450-52 | 070.987.720-03
+class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: true,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        })
+    }
+
+    validaCpf() {
+        if (!this.cpfLimpo) return false
+        if (typeof this.cpfLimpo !== 'string') return false
+        if (this.cpfLimpo === undefined) return false
+        if (this.cpfLimpo.length !== 11) return false
+        if (this.sequencia()) return false
+        
+        this.geraCpf()
+        return this.novoCpf === this.cpfLimpo
+    }
+
+    geraCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos)
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1)
+        this.novoCpf = cpfSemDigitos + digito1 + digito2
+    }
+
+    static geraDigito(cpfSemDigitos) {
+        const cpfArray = Array.from(cpfSemDigitos)
+        let regressivo = cpfArray.length + 1
+        const total = cpfArray.reduce((ac, valor) => {
+            ac += (regressivo * Number(valor))
+            console.log(`contador: ${regressivo} | cpf: ${valor} | acumulador: ${ac}`)
+            regressivo--
+            return ac
+        }, 0)
+
+        const digito = 11 - (total % 11)
+        console.log(`11 - (${total} % 11) = ${digito}`)
+        console.log(' ')
+        return digito <= 9 ? String(digito) : '0'
+    }
+
+    sequencia() {
+        return this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length) === this.cpfLimpo
+    }
+}
+
+const cpf = new ValidaCPF('070.987.720-03')
+
+if (cpf.validaCpf()) {
+    console.log('CPF válido!')
+} else {
+    console.log('CPF inválido!')
+}
