29ede2a580a851fa16b4789db526def423b1d705
jhonseloi <jhons.eloi@gmail.com>
2024-06-26 21:36:26 -0300

    atualização

--- curso-udemy/aula87/aula/js/validaCPF.js
+++ curso-udemy/aula87/aula/js/validaCPF.js
@@ -10,45 +10,36 @@ class ValidaCPF {
   }
 
   éSequência() {
-    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
   }
 
   geraNovoCpf() {
-    const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
-    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
-    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+    const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos)
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1)
     this.novoCPF = cpfSemDigitos + digito1 + digito2;
   }
 
   static geraDigito(cpfSemDigitos) {
     let total = 0;
-    let reverso = cpfSemDigitos.length + 1;
+    let reverso = cpfSemDigitos.length + 1
 
-    for(let stringNumerica of cpfSemDigitos) {
-      total += reverso * Number(stringNumerica);
+    for (let stringNumerica of cpfSemDigitos) {
+      total += reverso * Number(stringNumerica)
       reverso--;
     }
 
     const digito = 11 - (total % 11);
-    return digito <= 9 ? String(digito) : '0';
+    return digito <= 9 ? String(digito) : '0'
   }
 
   valida() {
-    if(!this.cpfLimpo) return false;
-    if(typeof this.cpfLimpo !== 'string') return false;
-    if(this.cpfLimpo.length !== 11) return false;
-    if(this.éSequência()) return false;
-    this.geraNovoCpf();
+    if (!this.cpfLimpo) return false;
+    if (typeof this.cpfLimpo !== 'string') return false
+    if (this.cpfLimpo.length !== 11) return false
+    if (this.éSequência()) return false
+    this.geraNovoCpf()
 
-    return this.novoCPF === this.cpfLimpo;
+    return this.novoCPF === this.cpfLimpo
   }
 }
-
-// let validacpf = new ValidaCPF('070.987.720-03');
-// // validacpf = new ValidaCPF('999.999.999-99');
-
-// if (validacpf.valida()) {
-//   console.log('CPF válido');
-// } else {
-//   console.log('CPF inválido');
-// }
