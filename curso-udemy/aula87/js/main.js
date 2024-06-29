caa08f492e1835398fede757fb6f2a52f7a800e9
jhonseloi <jhons.eloi@gmail.com>
2024-06-29 08:14:49 -0300

    atualização

--- curso-udemy/aula87/js/main.js
+++ curso-udemy/aula87/js/main.js
@@ -15,7 +15,7 @@ class ValidaFormulario {
     const camposValidos = this.camposSaoValidos()
     const senhasValidas = this.senhasSaoValidas()
 
-    if(camposValidos && senhasValidas) {
+    if (camposValidos && senhasValidas) {
       alert('Formulário enviado.')
       this.formulario.submit()
     }
@@ -23,63 +23,46 @@ class ValidaFormulario {
 
   camposSaoValidos() {
     let valid = true
-    
-    for(let errorText of this.formulario.querySelectorAll('.error-text')) {
+
+    for (let errorText of this.formulario.querySelectorAll('.error-text')) {
       errorText.remove()
     }
 
-    for(let campo of this.formulario.querySelectorAll('.validar')) {
+    for (let campo of this.formulario.querySelectorAll('.validar')) {
       const label = campo.previousElementSibling.innerText
-      
-      if(!campo.value) {
+
+      if (!campo.value) {
         this.criaErro(campo, `Campo "${label}" não pode estar em branco.`)
         valid = false
       }
-      
-      if(campo.classList.contains('cpf')) {
-        if(!this.validaCPF(campo)) valid = false
+
+      if (campo.classList.contains('cpf')) {
+        if (!this.validaCPF(campo)) valid = false
       }
-      
-      if(campo.classList.contains('usuario')) {
-        if(!this.validaUsuario(campo)) valid = false
+
+      if (campo.classList.contains('usuario')) {
+        if (!this.validaUsuario(campo)) valid = false
       }
     }
-    
+
     return valid
   }
-  
+
   senhasSaoValidas() {
     let valid = true
 
     const senha = this.formulario.querySelector('.senha')
     const repetirSenha = this.formulario.querySelector('.repetir-senha')
 
-    if(senha.value !== repetirSenha.value) {
+    if (senha.value !== repetirSenha.value) {
       valid = false
       this.criaErro(senha, 'Campos senha e repetir senha precisar ser iguais.')
       this.criaErro(repetirSenha, 'Campos senha e repetir senha precisar ser iguais.')
     }
 
-    if(senha.value.length < 6 || senha.value.length > 12) {
+    if (senha.value.length < 6 || senha.value.length > 12) {
       valid = false
       this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.')
-    }
-
-    return valid
-  }
-
-  validaUsuario(campo) {
-    const usuario = campo.value
-    let valid = true
-
-    if(usuario.length < 3 || usuario.length > 12) {
-      this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres.')
-      valid = false
-    }
-
-    if(!usuario.match(/^[a-zA-Z0-9]+$/g)) {
-      this.criaErro(campo, 'Nome de usuário precisar conter apenas letras e/ou números.')
-      valid = false
     }
 
     return valid
@@ -88,13 +71,31 @@ class ValidaFormulario {
   validaCPF(campo) {
     const cpf = new ValidaCPF(campo.value)
 
-    if(!cpf.valida()) {
+    if (!cpf.valida()) {
       this.criaErro(campo, 'CPF inválido.')
       return false
     }
 
     return true
   }
+
+  validaUsuario(campo) {
+    let valid = true
+    const usuario = campo.value
+
+    if (usuario.length < 3 || usuario.length > 12) {
+      this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres.')
+      valid = false
+    }
+
+    if (!usuario.match(/^[a-zA-Z0-9]+$/g)) {
+      this.criaErro(campo, 'Nome de usuário precisar conter apenas letras e/ou números.')
+      valid = false
+    }
+
+    return valid
+  }
+
 
   criaErro(campo, msg) {
     const div = document.createElement('div')
