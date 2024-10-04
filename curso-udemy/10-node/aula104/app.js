7bf87d103f579595bcb7fcbc94e739a0b8a08b3b
jhonseloi <jhons.eloi@gmail.com>
2024-10-03 23:05:53 -0300

    atualização

---
+++ curso-udemy/10-node/aula104/app.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const caminhoArquivo = path.resolve(__dirname, 'teste.json')
+const escreve = require('./modulos/escrever')
+
+const pessoas = [
+    { nome: 'João' },
+    { nome: 'Maria' },
+    { nome: 'José' },
+    { nome: 'Luiza' },
+]
+const json = JSON.stringify(pessoas, '', 4)
+escreve(caminhoArquivo, json)
