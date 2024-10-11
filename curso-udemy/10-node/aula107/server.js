e9e12d74391968cb9499969495df6982b91c2aad
jhonseloi <jhons.eloi@gmail.com>
2024-10-11 12:31:14 -0300

    atualização

--- curso-udemy/10-node/aula107/server.js
+++ curso-udemy/10-node/aula107/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded(
+    {
+     extended: true 
+    }
+))
+
 app.get('/', (req, res) => {
     res.send(`
         <form action="/" method="POST">
@@ -10,8 +16,15 @@ app.get('/', (req, res) => {
     )
 })
 
+app.get('/testes/:idUsuarios?/:parametro?/', (req, res) => {
+    console.log(req.params)
+    console.log(req.query)
+    res.send(req.params)
+})
+
 app.post('/', (req, res) => {
-    res.send('Recebi o formulário!')
+    console.log(req.body)
+    res.send(`O que você me enviou foi: "${req.body.nome}"`)
 })
 
 app.listen(1024, () => {
