b52206df917c97e5377654aab0a3189d9a60e2ef
jhonseloi <jhons.eloi@gmail.com>
2024-10-23 12:30:48 -0300

    atualização

--- curso-udemy/10-node/aula113/server.js
+++ curso-udemy/10-node/aula113/server.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+mongoose.connect(process.env.CONNECTIONSTRING)
+.then(() => {
+        app.emit('pronto')
+    })
+    .catch(e => console.log(e))
+
 const routes = require('./routes')
 const path = require('path')
 const meuMiddleware = require('./src/middlewares/middleware')
@@ -14,7 +23,9 @@ app.set('view engine', 'ejs')
 app.use(meuMiddleware)
 app.use(routes)
 
-app.listen(1024, () => {
-    console.log('Servidor executando na porta 1024.')
-    console.log('Acessar http://localhost:1024/')
+app.on('pronto', () => {
+    app.listen(1024, () => {
+        console.log('Servidor executando na porta 1024.')
+        console.log('Acessar http://localhost:1024/')
+    })
 })
