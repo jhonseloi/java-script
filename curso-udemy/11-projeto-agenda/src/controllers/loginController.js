cf3ec6483a79a46ca4f4ab06cd3b68478794d5c3
jhonseloi <jhons.eloi@gmail.com>
2024-11-14 18:31:29 -0300

    atualização

--- curso-udemy/11-projeto-agenda/src/controllers/loginController.js
+++ curso-udemy/11-projeto-agenda/src/controllers/loginController.js
@@ -1,4 +1,28 @@
+const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
     res.render('login')
     return
+}
+
+exports.register = async function(req, res) {
+    try {
+        const login = new Login(req.body)
+        await login.register()
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save(() => {
+                return res.redirect('/login/')
+            })
+            return
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso.') 
+        req.session.save(() => {
+            return res.redirect('/login/')
+        })
+    } catch(e) {
+        console.error('404')
+    }
 }
