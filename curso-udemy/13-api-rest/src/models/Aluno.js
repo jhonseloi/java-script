e9ccff2182f226ceb92dadee672fb3812b738c35
jhonseloi <jhons.eloi@gmail.com>
2024-12-25 13:27:54 -0300

    atualização

--- curso-udemy/13-api-rest/src/models/Aluno.js
+++ curso-udemy/13-api-rest/src/models/Aluno.js
@@ -10,8 +10,8 @@ export default class Aluno extends Model {
                     len: {
                         args: [3, 255],
                         msg: 'Nome precisa ter entre 3 e 255 caracteres.'
-                    }
-                }
+                    },
+                },
             },
             sobrenome: {
                 type: Sequelize.STRING,
@@ -20,8 +20,8 @@ export default class Aluno extends Model {
                     len: {
                         args: [3, 255],
                         msg: 'Nome precisa ter entre 3 e 255 caracteres.'
-                    }
-                }
+                    },
+                },
             },
             email: {
                 type: Sequelize.STRING,
@@ -33,7 +33,7 @@ export default class Aluno extends Model {
                     isEmail: {
                         msg: 'E-mail inválido.'
                     },
-                }
+                },
             },
             idade: {
                 type: Sequelize.INTEGER,
@@ -41,8 +41,8 @@ export default class Aluno extends Model {
                 validate: {
                     isInt: {
                         msg: 'Idade precisa ser um número inteiro.'
-                    }
-                }
+                    },
+                },
             },
             peso: {
                 type: Sequelize.FLOAT,
@@ -50,8 +50,8 @@ export default class Aluno extends Model {
                 validate: {
                     isFloat: {
                         msg: 'Peso precisa ser um número inteiro ou de ponto flutuante.'
-                    }
-                }
+                    },
+                },
             },
             altura: {
                 type: Sequelize.FLOAT,
@@ -59,9 +59,9 @@ export default class Aluno extends Model {
                 validate: {
                     isFloat: {
                         msg: 'Peso precisa ser um número inteiro ou de ponto flutuante.'
-                    }
-                }
-            }
+                    },
+                },
+            },
         }, {
             sequelize,
         })
