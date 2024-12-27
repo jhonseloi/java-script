fdb9498002bb8053b5fae2cac3416a9faefb4ad3
jhonseloi <jhons.eloi@gmail.com>
2024-12-27 12:31:13 -0300

    atualização

--- curso-udemy/13-api-rest/app.js
+++ curso-udemy/13-api-rest/app.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import { resolve } from 'path'
 
 import './src/database'
 import express from 'express'
@@ -20,6 +21,7 @@ class App {
     middlewares() {
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.json())
+        this.app.use(express.static(resolve(__dirname, 'uploads')))
     }
 
     routes() {
