21d643de0032ec193b9c5617133817ee701435e7
jhonseloi <jhons.eloi@gmail.com>
2025-01-22 13:07:42 -0300

    atualização

--- curso-udemy/14-react-basico/lista-de-tarefas/src/components/Main.js
+++ curso-udemy/14-react-basico/lista-de-tarefas/src/components/Main.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 
 // form
-import { FaPlus } from 'react-icons/fa' 
+import { FaPlus } from 'react-icons/fa'
+
+// tarefa
+import { FaEdit, FaWindowClose } from 'react-icons/fa'
 
 import './Main.css'
 
 export default class Main extends Component {
     state = {
-        novaTarefa: ''
+        novaTarefa: '',
+        tarefas: [],
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const { tarefas } = this.state
+        let { novaTarefa } = this.state
+        novaTarefa = novaTarefa.trim()
+
+        if(tarefas.indexOf(novaTarefa) !== -1) return
+
+        const novasTarefas = [...tarefas]
+
+        this.setState({
+            tarefas: [...novasTarefas, novaTarefa],
+        })
+    } 
 
     handleChange = (e) => {
         this.setState({
@@ -17,18 +36,30 @@ export default class Main extends Component {
     }
 
     render() {
-        const { novaTarefa } = this.state
+        const { novaTarefa, tarefas } = this.state
 
         return (
             <div className="main">
                 <h1>Lista de tarefas</h1>
 
-                <form action="#" className="form">
+                <form action="#" className="form" onSubmit={this.handleSubmit}>
                     <input type="text" onChange={this.handleChange} value={novaTarefa}/>
                     <button type="submit">
                         <FaPlus />
                     </button>
                 </form>
+
+                <ul className="tarefas">
+                    {tarefas.map((tarefa) => (
+                        <li key={tarefa}>
+                            {tarefa}
+                            <span>
+                                <FaEdit className="edit"/>
+                                <FaWindowClose className="delete" />
+                            </span>
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
