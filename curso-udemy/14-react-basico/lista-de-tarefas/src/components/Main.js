0e7450d4012baeec91e00928fc69865319fa3791
jhonseloi <jhons.eloi@gmail.com>
2025-01-23 18:28:09 -0300

    atualização

--- curso-udemy/14-react-basico/lista-de-tarefas/src/components/Main.js
+++ curso-udemy/14-react-basico/lista-de-tarefas/src/components/Main.js
@@ -12,11 +12,28 @@ export default class Main extends Component {
     state = {
         novaTarefa: '',
         tarefas: [],
+        index: -1,
+    }
+
+    componentDidMount() {
+        const tarefas = JSON.parse(localStorage.getItem('tarefas'))
+
+        if (!tarefas) return
+
+        this.setState({ tarefas })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { tarefas } = this.state
+
+        if (tarefas === prevState.tarefas) return
+
+        localStorage.setItem('tarefas', JSON.stringify(tarefas))
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        const { tarefas } = this.state
+        const { tarefas, index } = this.state
         let { novaTarefa } = this.state
         novaTarefa = novaTarefa.trim()
 
@@ -24,14 +41,44 @@ export default class Main extends Component {
 
         const novasTarefas = [...tarefas]
 
-        this.setState({
-            tarefas: [...novasTarefas, novaTarefa],
-        })
-    } 
+        if (index === -1) {
+            this.setState({
+                tarefas: [...novasTarefas, novaTarefa],
+                novaTarefa: '',
+            })
+        } else {
+            novasTarefas[index] = novaTarefa
+
+            this.setState({
+                tarefas: [...novasTarefas],
+                index: -1,
+                novaTarefa: '',
+            })
+        }
+    }
 
     handleChange = (e) => {
         this.setState({
             novaTarefa: e.target.value,
+        })
+    }
+
+    handleEdit = (e, index) => {
+        const { tarefas } = this.state
+
+        this.setState({
+            index,
+            novaTarefa: tarefas[index],
+        })
+    }
+
+    handleDelete = (e, index) => {
+        const { tarefas } = this.state
+        const novasTarefas = [...tarefas]
+        novasTarefas.splice(index, 1)
+
+        this.setState({
+            tarefas: [...novasTarefas]
         })
     }
 
@@ -50,12 +97,12 @@ export default class Main extends Component {
                 </form>
 
                 <ul className="tarefas">
-                    {tarefas.map((tarefa) => (
+                    {tarefas.map((tarefa, index) => (
                         <li key={tarefa}>
                             {tarefa}
                             <span>
-                                <FaEdit className="edit"/>
-                                <FaWindowClose className="delete" />
+                                <FaEdit onClick={(e) => this.handleEdit(e, index)} className="edit" />
+                                <FaWindowClose onClick={(e) => this.handleDelete(e, index)} className="delete" />
                             </span>
                         </li>
                     ))}
