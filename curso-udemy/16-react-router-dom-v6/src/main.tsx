42c2728e22858fbfcd60d1e947541494a0314e22
jhonseloi <jhons.eloi@gmail.com>
2025-04-07 18:19:30 -0300

    atualização

--- curso-udemy/16-react-router-dom-v6/src/main.tsx
+++ curso-udemy/16-react-router-dom-v6/src/main.tsx
@@ -4,13 +4,18 @@ import './styles/global.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import { About } from './components/About'
+import { Menu } from './components/Menu'
+import { Post } from './components/Post'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/posts/:id" element={<Post />} />
+        <Route path="/posts/" element={<Post />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
